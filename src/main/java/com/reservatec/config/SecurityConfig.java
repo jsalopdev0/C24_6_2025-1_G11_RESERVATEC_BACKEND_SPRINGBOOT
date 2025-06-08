112b8afd9c0e7f5d824ecf87995119370d6b7680
jsalopdev0 <jsalopdev@gmail.com>
2025-06-08 12:15:39 -0500

    cors-vite

--- src/main/java/com/reservatec/config/SecurityConfig.java
+++ src/main/java/com/reservatec/config/SecurityConfig.java
@@ -11,16 +11,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 @EnableMethodSecurity  // Habilita @PreAuthorize en los Controllers
 
 public class SecurityConfig {
     private final JwtFilter jwtFilter;
-    @Value("${cors.allowed-origin}")
-    private String allowedOrigin;
-
 
     public SecurityConfig(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
@@ -49,7 +45,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(allowedOrigin));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://reservatec-admin-31-220-104-112.traefik.me"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Content-Range"));
